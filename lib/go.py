29e387d823fcd148cf7020afdbe5b26a56293729
Christian Fobel <christian@fobel.net>
2018-02-11 00:03:39 -0500

    chore(version): bump version to 1.3.1

--- lib/go.py
+++ lib/go.py
@@ -58,7 +58,7 @@
 #   On Linux (or other UN*X systems) this is typically:
 #     ~/.go/shortcuts.xml
 
-__version_info__ = (1, 2, 1)
+__version_info__ = (1, 3, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
 from os.path import expanduser, exists, join
